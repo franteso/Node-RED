1ac966f3c150f42ddbea9b4c1f0abd9cbd4b6875
franteso <francoignacio.tedeschi@gmail.com>
2026-03-25 15:09:35 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,13 +3,11 @@ module.exports = {
         type: "credentials",
         users: [{
             username: "admin",
-            password: "$2b$10$6.172G8B.m76FzzS7paO9u.D7v.l15yO6.6D2hI2y0zV.66mZ.f1.",
+            password: "$2b$10$6.172G8B.m76FzzS7paO9u.D7v.l15yO6.6D2hI2y0zV.66mZ.f1.", // Clave: admin123
             permissions: "*"
         }]
     },
     uiPort: process.env.PORT || 1880,
-    mqttReconnectTime: 15000,
-    debugMaxLength: 1000,
     userDir: '/data',
     flowFile: 'flows.json',
     credentialSecret: "utn-sistemas-2026",
@@ -17,4 +15,4 @@ module.exports = {
     editorTheme: {
         projects: { enabled: false }
     }
-};
+}
