746d2a40eadc8dbd4dc62efcd3a91c169aa73cca
franteso <francoignacio.tedeschi@gmail.com>
2026-03-25 14:11:06 -0300

    Update admin username and credential secret

--- settings.js
+++ settings.js
@@ -1,22 +1,24 @@
 module.exports = {
+    // Seguridad obligatoria
     adminAuth: {
         type: "credentials",
         users: [{
-            username: process.env.NODE_RED_USERNAME || "admin",
+            username: process.env.NODE_RED_USERNAME || "franesp",
             password: process.env.NODE_RED_PASSWORD,
             permissions: "*"
         }]
     },
+    // Configuración básica para Railway
     uiPort: process.env.PORT || 1880,
     mqttReconnectTime: 15000,
-    serialReconnectTime: 15000,
     debugMaxLength: 1000,
     userDir: '/data',
     flowFile: 'flows.json',
-    credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET || "facu-utn-2026",
+    credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET || "utn-sistemas-2026",
     functionGlobalContext: { },
-    exportGlobalContextKeys: false,
+    // Forzamos el uso de la autenticación
     editorTheme: {
-        projects: { enabled: false }
+        projects: { enabled: false },
+        tours: false
     }
 };
