0b1eec6c8cbdefcca5f0142168adf4dade3b0332
franteso <francoignacio.tedeschi@gmail.com>
2026-03-25 14:59:31 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,16 @@ module.exports = {
         type: "credentials",
         users: [{
             username: "admin",
-            password: "$2b$10$6.172G8B.m76FzzS7paO9u.D7v.l15yO6.6D2hI2y0zV.66mZ.f1.", // Esto es: admin123
+            password: "$2b$10$6.172G8B.m76FzzS7paO9u.D7v.l15yO6.6D2hI2y0zV.66mZ.f1.",
             permissions: "*"
         }]
     },
-    uiPort: 1880,
+    uiPort: process.env.PORT || 1880,
+    mqttReconnectTime: 15000,
+    debugMaxLength: 1000,
     userDir: '/data',
     flowFile: 'flows.json',
-    credentialSecret: "clave-utn-2026",
+    credentialSecret: "utn-sistemas-2026",
     functionGlobalContext: { },
     editorTheme: {
         projects: { enabled: false }
